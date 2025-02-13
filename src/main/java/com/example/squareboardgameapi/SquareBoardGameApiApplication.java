e150b0f6e5df6538259cada34cab2e9f8e350d52
21JoaoMarques21 <joao.mendes@le-campus-numerique.fr>
2025-02-13 14:30:02 +0000

    Initial commit with Spring Boot setup

---
+++ src/main/java/com/example/squareboardgameapi/SquareBoardGameApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.squareboardgameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareBoardGameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareBoardGameApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/squareboardgameapi/SquareBoardGameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.squareboardgameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareBoardGameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
